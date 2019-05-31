f95ad80e3da803623a1d124467be234f62c9b3da
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-05-31 19:39:25 +0200

    project page added

---
+++ src/pages/projects.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const ProjectsPage = () => (
+  <div>
+    <h1>Projects by RüDev</h1>
+    <p>
+      Welcome to the project portfolio of Rüdev. Check out the ResumeBot!
+    </p>
+    <Link to="/">Go to index</Link>
+  </div>
+)
+
+export default ProjectsPage
