d53a1f5eaebb7abecf54378549f4bc73d3449553
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-05-31 19:35:53 +0200

    added back links to initial pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const AboutPage = () => (
+  <div>
+    <h1>About</h1>
+    <p>RüDev makes websites and develops products for clients.</p>
+    <Link to="/">Go to index</Link>
+  </div>
+)
+
+export default AboutPage
---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const AboutPage = () => (
+  <div>
+    <h1>RüBlog</h1>
+    <p>
+      Read all about it! Berlin web development company writes about web
+      development and more!
+    </p>
+    <Link to="/">Go to index</Link>
+  </div>
+)
+
+export default AboutPage
