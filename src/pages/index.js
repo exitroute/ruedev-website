31b6e73759fc2e78d4dad69376a9f7e0a79e3707
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-05-31 19:19:08 +0200

    initial pages and index links added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => (
+  <div>
+    <h1>RÜDEV</h1>
+    <p>Web Development From Berlin</p>
+
+    <div style={{
+      display: "flex"
+    }}>
+      <ul
+        style={{
+          listStyle: "none",
+        }}
+      >
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default IndexPage
