ab9934ab1ec4b934efc191937cb4bf02749e72d2
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-03 19:22:20 +0200

    Homepage title and nav centred using css grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,47 @@ import React from "react"
 import { Link } from "gatsby"
 
 const IndexPage = () => (
-  <div>
-    <h1>RÜDEV</h1>
-    <p>Web Development From Berlin</p>
-
-    <div style={{
-      display: "flex"
-    }}>
-      <ul
-        style={{
-          listStyle: "none",
-        }}
-      >
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-      </ul>
+  <div
+    style={{
+      display: "grid",
+      gridTemplateColumns: "1fr 300px 300px 1fr",
+      alignItems: "center",
+      height: "100vh",
+    }}
+  >
+    <div
+      style={{
+        gridColumnStart: "2",
+        textAlign: "center",
+      }}
+    >
+      <h1>RÜDEV</h1>
+      <p>Web Development From Berlin</p>
     </div>
+    <ul
+      style={{
+        gridColumnStart: "3",
+        textAlign: "center",
+        padding: "0",
+        listStyle: "none",
+      }}
+    >
+      <li>
+        <button>
+          <Link to="/about">About</Link>
+        </button>
+      </li>
+      <li>
+        <button>
+          <Link to="/projects">Projects</Link>
+        </button>
+      </li>
+      <li>
+        <button>
+          <Link to="/blog">Blog</Link>
+        </button>
+      </li>
+    </ul>
   </div>
 )
 
