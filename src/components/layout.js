577575215d60294dac1edbfb7ecfddb3ed94d350
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-06 13:28:57 +0200

    added layout component

---
+++ src/components/layout.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from 'gatsby';
+
+import "./layout.css"
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <div>
+          <main>{children}</main>
+          <footer>
+              © {new Date().getFullYear()}, Built with{` `}<a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+
 const AboutPage = () => (
-  <div>
+  <Layout>
     <h1>About</h1>
     <p>RüDev makes websites and develops products for clients.</p>
     <Link to="/">Go to index</Link>
-  </div>
+  </Layout>
 )
 
 export default AboutPage
