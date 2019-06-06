f0781ed9ab25e7f81793d488cd5f0f29fb754b47
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-06 12:25:23 +0200

    site metadata added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,9 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `RueDev Webdev`,
+    description: `A web development agency based in Berlin`,
+    author: `Ryan O'Shea`,
+  }
 }
