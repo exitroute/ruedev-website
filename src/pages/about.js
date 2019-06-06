a1c549fa09cffc15b325607fe8e26080042fb86b
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-06 20:26:40 +0200

    content

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,10 @@ const AboutPage = () => (
   <Layout>
     <h2>About RüDev</h2>
     <p>RüDev makes websites and develops products for clients.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias tenetur amet quo repellendus fugit, labore nostrum accusantium, officia fuga earum iusto inventore iste assumenda harum quaerat perspiciatis, necessitatibus animi a!</p>
     <h2>About Ryan</h2>
     <p>Ryan O'Shea makes websites and develops products for clients.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, assumenda dolorum. Delectus dignissimos non deserunt laborum tempore ipsa voluptatibus eum quisquam perspiciatis asperiores. Consequatur alias aperiam quo fuga dolore nostrum?</p>
     <Link to="/">Go to index</Link>
   </Layout>
 )
