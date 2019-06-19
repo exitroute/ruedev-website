b22edb24088ec684f89af3664c6ad5d0e9a79cb2
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-19 22:54:10 +0200

    layout changed

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,30 +4,30 @@ import { StaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 
 import Header from "./header"
+import Footer from "./footer"
 
 import "./layout.css"
 
+const Container = styled.div`
+  display: grid;
+  min-height: 100vh;
+  grid-template-rows: auto 1fr auto;
+`
 const Main = styled.main`
-  margin: 0 auto;
   padding: 0px 1.0875rem 1.45rem;
   padding-top: 0;
   section {
-    div {
-      padding: 1rem 0;
-    }
     display: grid;
+    grid-template-columns: minmax(auto, 600px);
     @media (min-width: 992px) {
-      grid-template-columns: 16rem 1fr;
+      grid-template-columns: 16rem minmax(auto, 600px);
       grid-auto-rows: auto;
       column-gap: 1rem;
       h2 {
         justify-self: end;
+        grid-column: 1 / 1;
       }
       p {
-        grid-column-start: 2;
-        max-width: 960px;
-      }
-      div {
         grid-column-start: 2;
       }
     }
@@ -46,8 +46,10 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header />
-        <Main>{children}</Main>
+        <Container>
+          <Header />
+          <Main>{children}</Main>
+          <Footer />
         </Container>
       </>
     )}
