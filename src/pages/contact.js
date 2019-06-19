5b3011a6f299b1cdb24f865eafb6e62db61e55d3
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-19 22:53:29 +0200

    contact chaged

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,6 +6,9 @@ const ContactPage = () => (
   <Layout>
     <section>
       <h2>Contact RüDev</h2>
+      <p>Ryan loves to hear from new people!</p>
+      <p>Drop him a line at: <a href="mailto: ryan@ruedev.com">ryan@ruedev.com</a></p>
+      <p>Or at any of these social media platroms:</p>
     </section>
   </Layout>
 )
