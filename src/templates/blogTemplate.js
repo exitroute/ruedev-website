9b85092833d1b0387a630a7e5fad59984a5544dc
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-19 22:53:59 +0200

    blogtemplate changed

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -8,24 +8,23 @@ const Author = styled.p`
   && {
     grid-column-start: 1;
     @media (min-width: 992px) {
-      text-align: right; 
+      text-align: right;
     }
   }
 `
 const Title = styled.h2`
   margin-bottom: 0.25rem;
 `
-export default function blogTemplate({ data,}) {
-  const { markdownRemark } = data 
+
+export default function blogTemplate({ data }) {
+  const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
       <section>
         <Title>{frontmatter.title}</Title>
         <Author>by {frontmatter.author}</Author>
-        <div
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+        <div dangerouslySetInnerHTML={{ __html: html }} />
       </section>
     </Layout>
   )
