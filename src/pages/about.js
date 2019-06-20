42504ec26d5f48d23e83d64bdbb634ac7f255b1e
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-20 18:42:13 +0200

    content added

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,14 +11,14 @@ const AboutPage = () => (
         the most from contemporary web development and design.
       </p>
       <p>
-        Rüdev was started by Ryan James O'Shea after pivoting his career, and
+        Rüdev was started by Ryan James O'Shea after forking his career, and
         then studying web development.
       </p>
       <p>
-        Just beofre he started the final project of his course, he realised that
-        any app he built would be most likely kinda useless. A typical learner
-        project. Better to use the time to start building an organisation, 
-        a company, a place to work.
+        Just before he started the final project of his course, he realised that
+        any app he sbuilt would be most likely kinda useless. A typical learner
+        project. Better to use the time to start building an organisation, a
+        company, a place to work.
       </p>
       <p>
         Rüdev works with small businesses, the self-employed, or anyone who
@@ -40,12 +40,30 @@ const AboutPage = () => (
     </section>
     <section>
       <h2>About Ryan</h2>
-      <p>Ryan O'Shea makes websites and develops products for clients.</p>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis,
-        assumenda dolorum. Delectus dignissimos non deserunt laborum tempore
-        ipsa voluptatibus eum quisquam perspiciatis asperiores. Consequatur
-        alias aperiam quo fuga dolore nostrum?
+        Ryan James O'Shea is a web developer living and working in Silicon
+        Allee, Berlin.
+      </p>
+      <p>
+        Software development is a irresistable way for him to combine and use
+        his experience as a manager, a designer, and trainer with 10,000 hours
+        in the classroom.
+      </p>
+      <p>
+        From that experience as a trainer and manager, he got to know the
+        importance of learning and performance. He wants to learn about his
+        clients needs, reason about those needs, and then to perform as
+        developer.
+      </p>
+      <p>
+        Before web development, Ryan had a 12 year career in edtech with Wall
+        Street English and Pearson Education, and has a training in
+        architectural design from the University of Strathcylde in Glasgow.
+      </p>
+      <p>
+        If he's running a workshop and needs a warm up where the introduction
+        needs a fun fact about him, he usual says something about: Sandor
+        Clegane, Pete Doherty or Sven Martinek.
       </p>
     </section>
   </Layout>
