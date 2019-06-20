441a6ed53da59e54c7b1971bf95d97116ab34b1d
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-20 16:50:28 +0200

    about ruedev added

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,12 +6,36 @@ const AboutPage = () => (
   <Layout>
     <section>
       <h2>About RüDev</h2>
-      <p>RüDev makes websites and develops products for clients.</p>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias
-        tenetur amet quo repellendus fugit, labore nostrum accusantium, officia
-        fuga earum iusto inventore iste assumenda harum quaerat perspiciatis,
-        necessitatibus animi a!
+        RüDev is a web development company that helps people and businesses get
+        the most from contemporary web development and design.
+      </p>
+      <p>
+        Rüdev was started by Ryan James O'Shea after pivoting his career, and
+        then studying web development.
+      </p>
+      <p>
+        Just beofre he started the final project of his course, he realised that
+        any app he built would be most likely kinda useless. A typical learner
+        project. Better to use the time to start building an organisation, 
+        a company, a place to work.
+      </p>
+      <p>
+        Rüdev works with small businesses, the self-employed, or anyone who
+        wants to get more from the internet and web development. We love to
+        explore and find what the problem and opportunity is, and then make what
+        is needed and what is useful.
+      </p>
+      <p>
+        Maybe you have a website and it's no longer meeting you needs, or maybe
+        you need a website but you are overwhelmed by the options. Rüdev will
+        help you decide on the best solution and deliver something custom made
+        that will
+      </p>
+      <p>Your experience with Rüdev is why Rüdev exists.</p>
+      <p>
+        We love doing this. We want you to enjoy the experience as much as we
+        do!
       </p>
     </section>
     <section>
